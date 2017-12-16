efb246148010fc978e71879264a5aa9515d65f61
ahmed esmat <ahmed esmat>
2017-12-16 20:33:02 +0200

    save text encrypted in file

--- Min_database.asm
+++ Min_database.asm
@@ -3,37 +3,76 @@ INCLUDE macros.inc
 .386
 .model flat,stdcall
 BUFFER_SIZE = 5001
+key = 251
 .data
 buffer byte BUFFER_SIZE DUP(?)
-filename byte "output.txt",0
+filename byte "database.txt",0
 filenamee BYTE 80 DUP(0)
 fileHandle HANDLE ?
 stringLength Dword ?
 bytesWritten Dword ?
 str1 BYTE "Cannot create file",0dh,0ah,0
-str2 BYTE "Bytes written to file [output.txt]:",0
+str2 BYTE "Bytes written to file [database.txt]:",0
 str3 BYTE "Enter up to 500 characters and press"
 BYTE "[Enter]: ",0
+
+enroll byte "Enter Student's ID and Grade seprated by @ :"
+;str1 byte "Enter Student Grade: ",0
+
 .code
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+main proc
+call  createfile_
+exit
+	
+main endp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+DisplayMessage PROC
+;display the message
+pushad
+call writestring
+;call Crlf
+popad
+ret
+DisplayMessage endp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+encrypt proc
+pushad
+mov ecx,stringLength
+mov esi,0
+Lo:
+xor buffer[esi],key ; translate a byte
+inc esi
+loop Lo
+popad
+ret 
+encrypt endp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 createfile_ proc
 ;create new file
+pushad
 mov edx, offset filename
 call createoutputfile
 mov fileHandle ,eax
 ; Check for errors.
-cmp eax, INVALID_HANDLE_VALUE ; error found?
+cmp eax, INVALID_HANDLE_VALUE      ; error found?
 jne file_ok ; no: skip
-mov edx,OFFSET str1 ; display error
-call WriteString
+mov edx,OFFSET str1                ; display error
+call DisplayMessage
 exit
 file_ok:
-; Ask the user to input a string
-mov edx, offset str3
-call writestring
+;Ask the user to input a string
+mov edx, offset enroll
+call DisplayMessage
 mov ecx,BUFFER_SIZE
 mov edx, offset buffer
 call readstring
 mov stringlength ,eax
+call encrypt
+
 ; Write the buffer to the output file.
 mov eax, filehandle
 mov edx,offset buffer
@@ -43,12 +82,15 @@ mov byteswritten ,eax
 call closefile
 ; Display the return value.
 mov edx,OFFSET str2 ; "Bytes written"
-call WriteString
+call DisplayMessage
 mov eax,bytesWritten
 call WriteDec
 call Crlf
+popad
 ret
 createfile_ endp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 Rfile proc
 ; Let user input a filename.
 mWrite "Enter an input filename: "
@@ -96,11 +138,6 @@ quit:
 exit
 ret
 Rfile endp
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-main proc
-;call  createfile_
-call Rfile
-exit
-	
-main endp
 end main
